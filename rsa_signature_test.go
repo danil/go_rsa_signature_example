8b3a22ecd971a778f8615de57ec5f78d42dbc06d
Danil Kutkevich <danil@kutkevich.org>
2018-12-16 22:49:50 +0300

    minor

--- rsa_signature_test.go
+++ rsa_signature_test.go
@@ -21,6 +21,38 @@ var testCases = []struct {
 	skip      string
 }{
 	{
+		name:      "verify",
+		index:     getLine(),
+		in:        []byte("пример"),
+		out:       true,
+		signature: "dchEsKOm7cdxW/LAPovvEe2iv6LUOlF4q1XpNSbxc61/0dCpm83liVBGnsTTufa2VWqslYEqyXlQFYOPXcLznTBYqTON8laskgd9LK/QXzUKnE3pErmzrgZ8dmGct5HLElTbuuja7iRAaBGB6ka6G8a6dZDfQfGEYtx+QEGg+og3TSSZdP9HAVeFlKmQOp9j5AY/r3q4ys0aWAMceIuR9C9vQ3q8h3WA53AxT5lmwhhP7QzIDwFY3oWj0ZpukleLQYAT1QN5rer0Q6ThsnO5DsxtnI5y1tLOIemsvRtoqUXRBFJSBO6g3im03XkvLzg9BerDJvwydGCmpTa9PFchYA==",
+		public: []byte(`-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnQtaNCjDySvY1Jci9LvA
+zaDUezB23i/NlHS43OWaYfI+bdJo0q2PPcopF2Ii9cuR5AacxyYQjncckGdPgnsO
+G7U3/cazeFUBF3O20SX9sV8Lgy5BOMrsnlmn4u9RDrViF67Vaxm+DNfBs1l51lCa
+JTIEzMzlmdkeNyS/Y/KqGDKxCLlTMiR3NRj1W9QWAuFj3U/4MgUv4dw7k4UJIKvv
+BuLTxxxLXuHVz8DvIbs0fRe5Ab7hS0J9Hxhd4K45nAjpKO+8YNkpAk9kMMDTDGXs
+BRKgvIQ1GbIle0LCVWCfKJYY07C18SGfGw9ACNHeQFvZkZwfh9J5XLvhTMNFhfc/
+PQIDAQAB
+-----END PUBLIC KEY-----`),
+	},
+	{
+		name:      "fail verification of the wrong word",
+		index:     getLine(),
+		in:        []byte("пример2"),
+		out:       false,
+		signature: "dchEsKOm7cdxW/LAPovvEe2iv6LUOlF4q1XpNSbxc61/0dCpm83liVBGnsTTufa2VWqslYEqyXlQFYOPXcLznTBYqTON8laskgd9LK/QXzUKnE3pErmzrgZ8dmGct5HLElTbuuja7iRAaBGB6ka6G8a6dZDfQfGEYtx+QEGg+og3TSSZdP9HAVeFlKmQOp9j5AY/r3q4ys0aWAMceIuR9C9vQ3q8h3WA53AxT5lmwhhP7QzIDwFY3oWj0ZpukleLQYAT1QN5rer0Q6ThsnO5DsxtnI5y1tLOIemsvRtoqUXRBFJSBO6g3im03XkvLzg9BerDJvwydGCmpTa9PFchYA==",
+		public: []byte(`-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnQtaNCjDySvY1Jci9LvA
+zaDUezB23i/NlHS43OWaYfI+bdJo0q2PPcopF2Ii9cuR5AacxyYQjncckGdPgnsO
+G7U3/cazeFUBF3O20SX9sV8Lgy5BOMrsnlmn4u9RDrViF67Vaxm+DNfBs1l51lCa
+JTIEzMzlmdkeNyS/Y/KqGDKxCLlTMiR3NRj1W9QWAuFj3U/4MgUv4dw7k4UJIKvv
+BuLTxxxLXuHVz8DvIbs0fRe5Ab7hS0J9Hxhd4K45nAjpKO+8YNkpAk9kMMDTDGXs
+BRKgvIQ1GbIle0LCVWCfKJYY07C18SGfGw9ACNHeQFvZkZwfh9J5XLvhTMNFhfc/
+PQIDAQAB
+-----END PUBLIC KEY-----`),
+	},
+	{
 		name:  "generate keys then sign and verify",
 		index: getLine(),
 		in:    []byte("пример"),
@@ -32,64 +64,32 @@ var testCases = []struct {
 		in:    []byte("пример"),
 		out:   true,
 		private: []byte(`-----BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEAy0fIwV+TZG7/1amjEx35A6yMKuZ4eY/79jEoAae1uGSn//Tb
-HEHkEeoMH8JA9yOhcykYS2ox3iAJ4mNhBGbrFGl5N1VGBhftjbbZNTV0OcGK2t0p
-KnpKdhTD3AfB3HOrquLTVYcZCBx6tQEk/1NMFQDK0JOzowqoZnlWqBcMhWyDmoLL
-Fr5IIwyiVZAlA3a62FL/1D5ke47vafzYZXZA7fOHUQX1vYojSLgSNPQ4Z/rzC998
-Nlj5B6jMed1qcFT7cQYyGk4R7Yc/6zcHd7uUhDCOK4xq4hQNdmwGhStQgHgJgWha
-rj6XjcebpNEJc9TSKB+U2igWThz2CNJd+i64tQIDAQABAoIBACEx8biSMPF/pstv
-Ti/6/wT4klMXG6lON+j2EgBjMKuR1FVZ2MX+hI3gWGZ/RDiXWvABT/RQvR/2v2Z2
-sAsV+2Fz79V3WV35XlN7/nBP8FDWKCShZO2I1sv9jBZMNf4X7XqUW8plq1LFw8v3
-CeGf6parcHMrC3Secu149wuv592JllBYtVBKugH/wCgQq0tmorL094Hka24jgeIC
-TqnI+TozgvfpJKJWomBBdwF+dJDMhYFqDgzQStOeTF2+tj1G0mIyIdPt4tPezvDh
-S4zR9nY0nKWiNadIhnWyJEFzYC4dIKjCK/wXEvrN6/QCmBcBh39ISumuE82XfoX6
-+oCsiykCgYEA3KkK3u681UY0SpBpcmeLkMpQWrlzgsHEbHfcQKNRu/iKd5WdRY+1
-X2aOkQDu2BizpijP7GkZc7Qhhc9heli8ozBS9AaeF/pJqIvqMQ7+MMD95KZ8O6yw
-1/PIYctJPoX9pL7UC6lc07cGhFw1S/+cju5ZLhftfQOh2UpaeqKeOkcCgYEA69Ys
-oMgtmWfUm4SkGwuhV6hOp22n4MFsW97wNX1a8FWynR4adEs5Kt1t+8xQPyxgRC5r
-9N6/++E1Rqo4GVTzTLLWdAppuhZproj252PfIpkq1/fQhj6RnxddOM4tPPPL/iSJ
-9QXcehH1487KblVrP+e7WGgq3w1CtFGLihP8tyMCgYEA2/k02hsbam2rMW4XgLJS
-wsu9IURhL8fk/dDbZCCsGXxi5WXkO/VQdUNy+oD4mbq+VPW4mdAITH0VMiUT4vjV
-TMWe3KBAOF0N0xVwKOnuY0HxmWCS1paIUTA44azbXpYKzpJiJyH2ZSS/PXICu7md
-JCveAYJfAVJ+pXRnEHxOyn8CgYB6uzj6a3KflRNl479re7/5WFOmekCVjNORdBHa
-JGSBEngjoBjwUH1tu1KTaZ6RYcyvbuErzAhpUqhhIBCVzknXrnJAbXLcLvHMrOah
-QK5M2R0cy5CvbDxoaMZQ26tbPxz4I5fP9b7poBMw/NsIVFe+KCCtP3igH8yI/q75
-qrEntQKBgDD6qizPwHrCwxl+vVbT0ZIvNGu6kSHDnJQNZuQjP4BC0d/flx0H9qMc
-fgd8jkt30nN5CqBZAguggF6Awno26DExnFGAgLKoNHg1jHTIOZeCHgJzW4slXyXY
-Z8fztgJFeN+KP6sJJxCDVdORSRPzFCKVW1pVKSETWWwTf+/drKHb
+MIIEogIBAAKCAQEAnQtaNCjDySvY1Jci9LvAzaDUezB23i/NlHS43OWaYfI+bdJo
+0q2PPcopF2Ii9cuR5AacxyYQjncckGdPgnsOG7U3/cazeFUBF3O20SX9sV8Lgy5B
+OMrsnlmn4u9RDrViF67Vaxm+DNfBs1l51lCaJTIEzMzlmdkeNyS/Y/KqGDKxCLlT
+MiR3NRj1W9QWAuFj3U/4MgUv4dw7k4UJIKvvBuLTxxxLXuHVz8DvIbs0fRe5Ab7h
+S0J9Hxhd4K45nAjpKO+8YNkpAk9kMMDTDGXsBRKgvIQ1GbIle0LCVWCfKJYY07C1
+8SGfGw9ACNHeQFvZkZwfh9J5XLvhTMNFhfc/PQIDAQABAoIBACmxVN5aIDhtWqB+
+C3q7yeENnLujzzsHp+WM43NJxaXRQT+4x7l++HNoE2aw2CU3SWEnXEIG2ghomP3B
+X7t9Xe5/OwE12nnM34BRaSy3kFWhrRXDlu8z+IPFu6uk63kjMIqnEOPhLrKMKVGr
+JIyAU5wiXmHc1+vzV3E+YU1wpDXN/PWNZbfu0xdEmfG9pEA3IOXkv5H/nRrPnirK
+SzsrS/zyivz/W+3XLUwLHMf/wSBti9nl5Ezo9dnb+I2lknXH2NetR+c/0OvSj7ld
+c08eq8BWXyNFQvkZaTsFhlJdjFcxcKDytlNFlseZlIBO3JFNsJhOKTV4xg1ldmzT
+HGUilyECgYEAy8r1ndxAcr8SIJp3DlIn436ZwrUhzQn8rFMMzUihYTW+XI0Tj8C5
+gU8f0E0O4xvEKEk4YT/mdwAmnPW6FX0NbPd5jg+qSJ/R6oOAYYUMVe3J/4Emg0lL
+xphEE59HddoreB2r/fI6Au22ssHTrb1eqIG42DEAEN3MUILFFhhvoaUCgYEAxUaO
+0vavua+L1oBOcvfurKC4v18mSW1Db2Bcmf0ciBM6RaYkyub1VMOZYjtKtO3QzWwQ
+K98VLpXrlSLVavJSiSIkoVbhoNAkhjLzXUKFQgDUQPtEpg18EXU8Xv7I6xY21sM9
+p+oDSQ7PZCeeUXerqmtDHWG1zCXWimJqsasEg7kCgYBgBVYrNh7Lsgl5gS49eio/
+P2R1YGD3ug/qpgrvIpyfL/JsqzAfWIBPVBe6TsSH74pCLRW6hKAzS8flxsYR3+UW
+hZgpfmoOY/dFVwaDbGv+hNbbY1/hFgT1IsNYt4C1H8HBi8GBGsOIo7akjT0OrJ5Q
+KScN/jB4wfjFqofwPD1E3QKBgDCg+vX//0M7hbIDgNMQUNJW8BOkhdCV0Yiz7T+8
+R/s3GicvFGS71//kE3xGd1zwPodUuwvFevg31pG1u3wYbcYGF5d5zjaQ+F/oUVtM
+2YJyp9+40KxKKKo5Bv+3uJlSqYP5LsqkgdjRgMgEiB8266cft7SXcHP5Y9BLAFf5
+eyxpAoGAM50bgJ32u2IhO1/ZGFPD78c2dTAvHncyaZm2/jFf7Y9galEx07FPfRWF
+6AOd0GOhoDsMHjAnx/2BQI9lAdoI3iFVbp3Lg/WB6WR4/96Wy4/w551XI2JnJnON
+oVL8VxqMtRXhbgr9NxX0qF8HmMqQhxIDwmOv4dIgqSuq2QWrcCM=
 -----END RSA PRIVATE KEY-----`),
-	},
-	{
-		name:      "verify",
-		index:     getLine(),
-		in:        []byte("пример"),
-		out:       true,
-		signature: "LcZpeFAi25p8tYL5ANx+LG4ZrOxyukD9eqMf5h8AJgGbr/hhBlpcbl+ZNLd2nsEdknPtsRYmd2Ac+6SDAh9BUPfLslZMP0tS38AdIyfxN86fP1ZhxUn/m91x8krS/eiqxpq9GY1wTboK08ojtHmUCZmyCbFOwY2Fmf86rY4FYJDE8dUeDOluoQGDUK4upjnrcaLnxcWahzEnAkcbI36zK9fvhaGM0eN2KQEPqSUBJE4O2oR0J0MIzFQrUsCiSlZFD6Mt62VVjzuSeeY6C/3YODsz+2YiaJa5uXn5wQXBk4cbjk+G5+Hw2R5p1vJxAfwO1/q2h16WLLiyg+Dn/pvPMA==",
-		public: []byte(`-----BEGIN RSA PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy0fIwV+TZG7/1amjEx35
-A6yMKuZ4eY/79jEoAae1uGSn//TbHEHkEeoMH8JA9yOhcykYS2ox3iAJ4mNhBGbr
-FGl5N1VGBhftjbbZNTV0OcGK2t0pKnpKdhTD3AfB3HOrquLTVYcZCBx6tQEk/1NM
-FQDK0JOzowqoZnlWqBcMhWyDmoLLFr5IIwyiVZAlA3a62FL/1D5ke47vafzYZXZA
-7fOHUQX1vYojSLgSNPQ4Z/rzC998Nlj5B6jMed1qcFT7cQYyGk4R7Yc/6zcHd7uU
-hDCOK4xq4hQNdmwGhStQgHgJgWharj6XjcebpNEJc9TSKB+U2igWThz2CNJd+i64
-tQIDAQAB
------END RSA PUBLIC KEY-----`),
-	},
-	{
-		name:      "fail verification of the wrong word",
-		index:     getLine(),
-		in:        []byte("пример2"),
-		out:       false,
-		signature: "LcZpeFAi25p8tYL5ANx+LG4ZrOxyukD9eqMf5h8AJgGbr/hhBlpcbl+ZNLd2nsEdknPtsRYmd2Ac+6SDAh9BUPfLslZMP0tS38AdIyfxN86fP1ZhxUn/m91x8krS/eiqxpq9GY1wTboK08ojtHmUCZmyCbFOwY2Fmf86rY4FYJDE8dUeDOluoQGDUK4upjnrcaLnxcWahzEnAkcbI36zK9fvhaGM0eN2KQEPqSUBJE4O2oR0J0MIzFQrUsCiSlZFD6Mt62VVjzuSeeY6C/3YODsz+2YiaJa5uXn5wQXBk4cbjk+G5+Hw2R5p1vJxAfwO1/q2h16WLLiyg+Dn/pvPMA==",
-		public: []byte(`-----BEGIN RSA PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAy0fIwV+TZG7/1amjEx35
-A6yMKuZ4eY/79jEoAae1uGSn//TbHEHkEeoMH8JA9yOhcykYS2ox3iAJ4mNhBGbr
-FGl5N1VGBhftjbbZNTV0OcGK2t0pKnpKdhTD3AfB3HOrquLTVYcZCBx6tQEk/1NM
-FQDK0JOzowqoZnlWqBcMhWyDmoLLFr5IIwyiVZAlA3a62FL/1D5ke47vafzYZXZA
-7fOHUQX1vYojSLgSNPQ4Z/rzC998Nlj5B6jMed1qcFT7cQYyGk4R7Yc/6zcHd7uU
-hDCOK4xq4hQNdmwGhStQgHgJgWharj6XjcebpNEJc9TSKB+U2igWThz2CNJd+i64
-tQIDAQAB
------END RSA PUBLIC KEY-----`),
 	},
 }
 
